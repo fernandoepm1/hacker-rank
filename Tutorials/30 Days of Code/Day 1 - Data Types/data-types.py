24a7894db7314a67365f210d8bd8e82fdc01aa4f
Fernando Eufrásio <fernando.epm1@gmail.com>
2019-10-21 13:08:46 -0300

    Add Day 1 and Day 2 in python and ruby

---
+++ Tutorials/30 Days of Code/Day 1 - Data Types/data-types.py
@@ -0,0 +1,14 @@
+# Variables declared by default
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+# Get the other variables as user input
+integer_num = int(input())
+double_num = float(input())
+string = input()
+
+# Print the sum of the variables on the screen
+print(i + integer_num)
+print(d + double_num)
+print(s + string)
---
+++ Tutorials/30 Days of Code/Day 1 - Data Types/data-types.rb
@@ -0,0 +1,14 @@
+# Variables declared by default
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+# Get the other variables as user input
+integer_num = gets.to_i
+double_num = gets.to_f
+string = gets
+
+# Print the sum of the variables on the screen
+puts i + integer_num
+puts d + double_num
+puts s + string
